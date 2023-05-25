c8a82baf37e4725688226a85567158820706a1ee
meerasndr <mirage.sundial@gmail.com>
2023-05-25 08:31:09 +0530

    replace question with prompttemplate to make the app more user-driven

--- script.py
+++ script.py
@@ -6,27 +6,32 @@ from langchain.llms import OpenAI
 from langchain import PromptTemplate
 from langchain import LLMChain
 from fastapi import FastAPI
+import uvicorn
 app = FastAPI()
 
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-question = "Can you list 10 carnatic ragas as a comma separated list? This is the format raga1, raga2, raga3.."
+template = "Can you list {n} carnatic ragas as a comma separated list? This is the format raga1, raga2, raga3.."
 llm = OpenAI(model_name="text-davinci-003")
+prompt = PromptTemplate(template=template, input_variables=['n'])
+chain = LLMChain(llm=llm, prompt=prompt)
+input = {'n': 5}
 
 @app.get("/")
 async def root():
 	try:
-		raga_list_csv = llm(question)
+		raga_list_csv = chain.run(input)
 	except:
 		print("There's an unforeseen error. Try again later. \n Thanks for your patience. ")
 		exit(0)
 	raga_list = raga_list_csv.replace(" ", "").strip().split(',')
 	message = ''
-	#for item in raga_list:
-		#message += item
 	return {"message": raga_list}
+
+if __name__ == "__main__":
+	uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 
