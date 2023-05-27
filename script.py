14842ad306cea29863846094ed42abffde04c602
meerasndr <mirage.sundial@gmail.com>
2023-05-27 07:22:52 +0530

    add code to handle raga number and cors

--- script.py
+++ script.py
@@ -5,8 +5,17 @@ from langchain.llms import OpenAI
 from langchain import PromptTemplate
 from langchain import LLMChain
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import uvicorn
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this list based on your requirements
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 load_dotenv()
@@ -16,15 +25,23 @@ template = "Can you list {n} carnatic ragas as a comma separated list? This is t
 llm = OpenAI(model_name="text-davinci-003")
 prompt = PromptTemplate(template=template, input_variables=['n'])
 chain = LLMChain(llm=llm, prompt=prompt)
-input = {'n': 5}
 
 @app.get("/")
 async def root():
+	return {"message": "Hello"}
+
+@app.post("/{num_ragas}")
+async def post(num_ragas:int):
 	try:
-		raga_list_csv = chain.run(input)
+		if num_ragas < 21 and num_ragas > 1:
+			input = {'n': num_ragas}
+			raga_list_csv = chain.run(input)
+		else:
+			return {"message": "We are able to give you upto 20 ragas at a time. Enter a number less than 21 and more than 1"}
+
 	except:
-		print("There's an unforeseen error. Try again later. \n Thanks for your patience. ")
-		exit(0)
+		return {"message" : "There's an unforeseen error. Try again later. \n Thanks for your patience. "}
+
 	raga_list = raga_list_csv.replace(" ", "").strip().split(',')
 	return {"message": raga_list}
 
